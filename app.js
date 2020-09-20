4550697c180db632184c9911cdf81c0b28a36fe0
CurvingSphinx4 <flodareltih90@gmail.com>
2020-09-20 18:48:26 +0200

    Now saves Stories and edit is available

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require("express")
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
 
 // load config
@@ -17,25 +19,45 @@ connectDB()
 
 const app = express()
 
+// body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 // logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"))
 }
 
+// handlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 // handlebars 
-app.engine('.hbs', exphbs({defaultLayout: "main", extname: '.hbs'}));
+app.engine('.hbs', exphbs({ helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
+}, defaultLayout: "main", extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // sesion middleware
 app.use(session({
     secret: 'storybooks',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
 // passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// global variables
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -43,6 +65,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // routes 
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
---
+++ routes/stories.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+// @desc show add page
+// @route GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+    res.render('stories/add')
+})
+
+// @desc process add form
+// @route POST /stories
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+// @desc show all stories
+// @route GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: "Public" })
+        .populate('user')
+        .sort({ createdAt: 'desc' })
+        .lean()
+
+        res.render('stories/index', {
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+// @desc show edit stories page
+// @route GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    const story = await Story.findOne({ _id: req.params.id }).lean()
+    if (!story) {
+        res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+        req.redirect('/stories')
+    } else {
+        res.render('stories/edit', {
+            story
+        })
+    }
+})
+
+module.exports = router
