8a0bbea011576b6826c11f3a4c455665bb07bb12
CurvingSphinx4 <flodareltih90@gmail.com>
2020-09-20 19:56:05 +0200

    Completed

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,27 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc show single story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id)
+        .populate('user')
+        .lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        res.render('stories/show', {
+            story
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
 // @desc show edit stories page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -90,6 +111,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     try {
         await Story.remove({ _id: req.params.id })
         res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+// @desc show user stories
+// @route GET /stories/user/:id
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: "Public"
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index', {
+            stories
+        })
     } catch (err) {
         console.error(err)
         return res.render('error/500')
