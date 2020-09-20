51ab6d752bbb88f8163f426117a369e4f5a7c585
CurvingSphinx4 <flodareltih90@gmail.com>
2020-09-20 15:57:40 +0200

    Major Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const express = require("express")
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
+
+// load config
+dotenv.config({ path: './config/config.env' })
+
+// passpoort config
+require('./config/passport')(passport);
+
+connectDB()
+
+const app = express()
+
+// logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+// handlebars 
+app.engine('.hbs', exphbs({defaultLayout: "main", extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// sesion middleware
+app.use(session({
+    secret: 'storybooks',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// routes 
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+)
